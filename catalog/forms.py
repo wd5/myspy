36edf869ab8316f5a3a8e046106d26f9e067c41f
Vladimir PAL <freebsdstuff@MacBook-Vladimir-PAL.local>
2011-03-05 15:47:36 +0300

    Верстка

--- catalog/forms.py
+++ catalog/forms.py
@@ -42,7 +42,7 @@ class OrderForm(forms.Form):
     def as_table(self):
         "Returns this form rendered as HTML <tr>s -- excluding the <table></table>."
         return self._html_output(
-            normal_row = u'<tr%(html_class_attr)s><th>%(label)s</th><td colspan="2" class="send_input">%(errors)s%(field)s%(help_text)s</td></tr>',
+            normal_row = u'<tr%(html_class_attr)s><th>%(label)s</th><td colspan="2" class="text">%(errors)s%(field)s%(help_text)s</td></tr>',
             error_row = u'<tr><td colspan="2"> class="send_input"%s</td></tr>',
             row_ender = u'</td></tr>',
             help_text_html = u'<br />%s',
