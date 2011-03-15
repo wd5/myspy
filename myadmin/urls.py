e4200d461aee6e05ee274df3a06fada4ee3ee016
Vladimir PAL <freebsdstuff@MacBook-Vladimir-PAL.local>
2011-03-15 20:32:46 +0300

    Бухгалтерия

--- myadmin/urls.py
+++ myadmin/urls.py
@@ -3,5 +3,5 @@ from django.conf.urls.defaults import *
 urlpatterns = patterns('',
                       url(r'^$', 'myadmin.views.auth', name="auth-page"),
                       url(r'^sales$', 'myadmin.views.sales', name="auth-page"),
-                      url(r'^sales/edit/(?P<id>[-\w]+)/$', 'myadmin.views.edit_client', name="client-id"),)
+                      url(r'^client/(?P<id>[-\w]+)/$', 'myadmin.views.edit_client', name="client-id"),)
 
