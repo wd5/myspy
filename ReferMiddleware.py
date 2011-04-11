5734a51bb4fa5596cde453d61b97936f40cdf42b
Владимир <vladimir@MacBook-Pro-Vladimir.local>
2011-04-11 20:31:43 +0400

    Bug bix

--- ReferMiddleware.py
+++ ReferMiddleware.py
@@ -6,7 +6,7 @@ class ReferMiddleware(object):
     def process_response(self, request, response):
         if not request.COOKIES.get('REFERRER', None):
             referrer = request.META.get('HTTP_REFERER', '')
-            max_age = request.session.get_expiry_age()
+            max_age = 7*24*60*60 # week
             expires_time = time.time() + max_age
             expires = cookie_date(expires_time)
             response.set_cookie('REFERRER', referrer, max_age=max_age, expires=expires)
