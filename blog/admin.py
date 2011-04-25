add35227a4855d46fec4977f2a4537146c154ad0
Владимир <vladimir@MacBook-Pro-Vladimir.local>
2011-04-25 20:00:47 +0400

    Bug bix

--- blog/admin.py
+++ blog/admin.py
@@ -1,6 +1,6 @@
-from django.contrib import admin
-from blog.models import Entry, Category
+#from django.contrib import admin
+#from blog.models import Entry, Category
 
-admin.site.register(Entry)
-admin.site.register(Category)
+#admin.site.register(Entry)
+#admin.site.register(Category)
 
