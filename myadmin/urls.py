7c5291e399c0d20320d87446525fcc5b89350803
Владимир <vladimir@MacBook-Pro-Vladimir.local>
2011-04-25 20:07:17 +0400

    Bug bix

--- myadmin/urls.py
+++ myadmin/urls.py
@@ -7,8 +7,8 @@ urlpatterns = patterns('',
                       url(r'^client/(?P<id>[-\w]+)/delete$', 'myadmin.views.delete_client', name="delete-client"),
                       url(r'^client/add$', 'myadmin.views.add_client', name="add-client"),
                       url(r'^store$', 'myadmin.views.store', name="store-page"),
+                      url(r'^cash/add/$', 'myadmin.views.add_cashflow', name="cash-page"),
                       url(r'^cash/(?P<when>[-\w]+)/$', 'myadmin.views.cash', name="date_cash-page"),
                       url(r'^cash/edit/(?P<id>[-\w]+)/$', 'myadmin.views.edit_cashflow', name="edit-cashflow"),
-                      url(r'^cash/add/$', 'myadmin.views.add_cashflow', name="cash-page"),
                       url(r'^cash/editbalance$', 'myadmin.views.edit_balance', name="edit-balance"),)
 
