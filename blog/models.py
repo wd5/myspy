ac03b9b92c4c93ffd646d9b04f37691266a426b3
Владимир <vladimir@MacBook-Pro-Vladimir.local>
2011-04-29 20:08:54 +0400

    Bug bix

--- blog/models.py
+++ blog/models.py
@@ -1,15 +1,15 @@
-from django.db import models
-from tinymce import models as tinymce_models
-
-class Category(models.Model):
-    name = models.CharField(max_length=100)
-    slug = models.SlugField(max_length=50, unique=True)
-
-class Entry(models.Model):
-    title = models.CharField(max_length=100)
-    slug = models.SlugField(max_length=50, unique=True)
-    category = models.ManyToManyField(Category)
-    date = models.DateField()
-    entry = tinymce_models.HTMLField()
-    thumbnail_entry = tinymce_models.HTMLField()
+#from django.db import models
+#from tinymce import models as tinymce_models
+#
+#class Category(models.Model):
+#    name = models.CharField(max_length=100)
+#    slug = models.SlugField(max_length=50, unique=True)
+#
+#class Entry(models.Model):
+#    title = models.CharField(max_length=100)
+#    slug = models.SlugField(max_length=50, unique=True)
+#    category = models.ManyToManyField(Category)
+#    date = models.DateField()
+#    entry = tinymce_models.HTMLField()
+#    thumbnail_entry = tinymce_models.HTMLField()
 
