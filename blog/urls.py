13d2fb469606d61eb7445ffd293689dada54267f
Владимир <vladimir@MacBook-Pro-Vladimir.local>
2011-04-30 12:10:42 +0400

    Bug bix

--- blog/urls.py
+++ blog/urls.py
@@ -1,5 +1,5 @@
 from django.conf.urls.defaults import *
 
 urlpatterns = patterns('',
-                          url(r'^$', 'myspy.blog.views.blog', name="main-page"),
+                          url(r'^$', 'blog.views.blog', name="main-page"),
 )
--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render_to_response
 from django.template import RequestContext
-from myspy.blog.models import Entry
+from blog.models import Entry
 
 def blog(request):
     entrys = Entry.objects.all()
