c34b2c6d94ec3875f167ac7e56739d32b30f6596
Владимир <vladimir@MacBook-Pro-Vladimir.local>
2011-08-10 15:26:31 +0400

    Bug bix

--- cart/models.py
+++ cart/models.py
@@ -107,7 +107,7 @@ class Client(models.Model):
         cart_items = CartProduct.objects.filter(cartitem = self.cart.id)
         products = ""
         for item in cart_items:
-            products += u"<a href=\"%s\" target=\"_blank\">%s</a> - %sшт; " % (item.product.get_absolute_url(), item.product.slug, item.quantity )
+            products += u"<a href=\"%s\" target=\"_blank\">%s</a> - %sшт<br> " % (item.product.get_absolute_url(), item.product.slug, item.quantity )
         return products
 
     class Meta:
