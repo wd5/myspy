ccd0eb6a93b9281cf6637c2f6a8ccaae65d29875
Владимир <vladimir@MacBook-Pro-Vladimir.local>
2011-09-29 22:30:07 +0400

    Bug bix

--- myadmin/management/commands/test_command.py
+++ myadmin/management/commands/test_command.py
@@ -1,11 +1,10 @@
 from django.core.management.base import BaseCommand
-from cart.models import Client
-import urllib
+from myadmin.models import Cash
 
 class Command(BaseCommand):
     def handle(self, *args, **options):
-        clients = Client.objects.all()
-        for client in clients:
-            if client.referrer:
-                print len(str(client.referrer))
-                print urllib.unquote(client.referrer.encode('utf8'))
+        cashs = Cash.objects.filter(cause="SALARY_VICTOR")
+        b = 0
+        for i in cashs:
+            b -= i.cashflow
+        print b
