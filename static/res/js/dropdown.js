630eb44ec36f42ac0abe98bb945be4c490a36327
Владимир <vladimir@MacBook-Pro-Vladimir.local>
2012-02-08 22:56:09 +0400

    Bug bix

--- static/res/js/dropdown.js
+++ static/res/js/dropdown.js
@@ -1,15 +1,14 @@
 // DROP DOWN BY PAGELINES
 
-var $j = jQuery.noConflict();
 
-$j(document).ready(function () {
+$(document).ready(function () {
 	
-	$j("ul.dropdown > li").hover(function(){
-        $j(this).addClass("menuhover");
-        $j(this).find('#header_sprite').fadeIn(0);
+	$("ul.dropdown > li").hover(function(){
+        $(this).addClass("menuhover");
+        $(this).find('#header_sprite').fadeIn(0);
     }, function(){	
-        $j(this).removeClass("menuhover");
-        $j(this).find('#header_sprite').fadeOut(0);
+        $(this).removeClass("menuhover");
+        $(this).find('#header_sprite').fadeOut(0);
 
     });
 
