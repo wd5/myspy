17b1887a10f42c16a23747267faf9bb7318f1072
Server <myspyshop@gmail.com>
2012-05-21 17:16:19 +0400

    Bug fix

--- cart/cart.py
+++ cart/cart.py
@@ -198,7 +198,7 @@ def send_client_email(cart_items, form, cart_subtotal):
 def send_sms(cart_items, form):
     login = 'palv1@yandex.ru'
     password = '97ajhJaj9zna'
-    phones = ["79151225291", "79267972292"]
+    phones = ["79257400502","79267972292",]
     from_phone = re.sub("\D", "", form.cleaned_data['phone'])
     products = ""
     for item in cart_items:
@@ -217,4 +217,4 @@ def send_sms(cart_items, form):
             msg = urllib.urlencode({'msg': msg.encode('cp1251')})
             urllib2.urlopen('http://sms48.ru/send_sms.php?login=%s&to=%s&%s&from=%s&check2=%s' % (login, to_phone, msg.encode('cp1251'), '79151225291', md5(login + md5(password).hexdigest() + to_phone).hexdigest()) )
         else:
-            urllib2.urlopen('http://sms48.ru/send_sms.php?login=%s&to=%s&%s&from=%s&check2=%s' % (login, to_phone, msg.encode('cp1251'), 'my-spy', md5(login + md5(password).hexdigest() + to_phone).hexdigest()) )
+            urllib2.urlopen('http://sms48.ru/send_sms.php?login=%s&to=%s&%s&from=%s&check2=%s' % (login, to_phone, msg.encode('cp1251'), from_phone, md5(login + md5(password).hexdigest() + to_phone).hexdigest()) )
